59bb24994783170099b87547e9e45c193027a62d
Hammad Ali <ch.hammad2914@gmail.com>
2023-07-10 11:52:10 +0500

    Update 2.0

--- js/ipStack.js
+++ js/ipStack.js
@@ -1,15 +1,36 @@
-// // set endpoint and your access key
-// var ip = '134.201.250.155'
-// var access_key = 'd58325704a33a57af26180c8cb4128ff';
 
-// // get the API result via jQuery.ajax
-// $.ajax({
-//     url: 'http://api.ipstack.com/' + ip + '?access_key=' + access_key,
-//     dataType: 'jsonp',
-//     success: function(json) {
+// const accessKey = 'd58325704a33a57af26180c8cb4128ff';
 
-//         // output the "capital" object inside "location"
-//         console.log(json)
+// // Function to retrieve visitor information
+// function getVisitorInformation() {
+//   // Make an HTTP GET request to the IPStack API endpoint
+//   fetch(`http://api.ipstack.com/check?access_key=${accessKey}`)
+//     .then(response => response.json())
+//     .then(data => {
+//       // Access the visitor information from the response data
+//       const ip = data.ip;
+//       const country = data.country_name;
+//       const region = data.region_name;
+//       const city = data.city;
+//       const zip = data.zip;
+//       const latitude = data.latitude;
+//       const longitude = data.longitude;
+//       const info = data;
 
-//     }
-// });
+//     //   Display the visitor information
+//       console.log('IP:', ip);
+//       console.log('Country:', country);
+//       console.log('Region:', region);
+//       console.log('City:', city);
+//       console.log('ZIP Code:', zip);
+//       console.log('Latitude:', latitude);
+//       console.log('Longitude:', longitude);
+//     // console.log(info);
+//     })
+//     .catch(error => {
+//       console.error('Error:', error);
+//     });
+// }
+
+// // Call the function to retrieve visitor information
+// getVisitorInformation();
--- js/translate.js
+++ js/translate.js
@@ -1,20 +1,52 @@
-$('#MM-Language-FR').click(function(e){
-    e.stopPropagation();
-    translate();
+// Replace YOUR_API_KEY with your actual API key
+var apiKey = 'AIzaSyACEeMkAF2kq1w14nCFttKZBKVUrY9C3NE';
+
+// The target language code
+var targetLanguage = 'fr'; // French
+
+// Function to translate the text using the Translation API
+function translateText(text, callback) {
+  // Make an AJAX request to the Translation API
+  $.ajax({
+    url: 'https://translation.googleapis.com/language/translate/v2?key=' + apiKey,
+    type: 'POST',
+    data: {
+      q: text,
+      target: targetLanguage
+    },
+    dataType: 'json',
+    success: function(response) {
+      // Handle the response
+      var translatedText = response.data.translations[0].translatedText;
+      callback(null, translatedText);
+    },
+    error: function(error) {
+      callback(error);
+    }
   });
+}
+
+// Function to translate the entire web page
+function translatePage() {
+  // Get all text elements in the web page
+  var elements = document.querySelectorAll('body *:not(script)');
   
-  function translate(){
-    var sourceText = $('.path-frontpage').html();
-    var targetLang = 'fr';
+  // Translate each text element
+  elements.forEach(function(element) {
+    var text = element.textContent;
     
-    var url = "https://translate.googleapis.com/translate_a/single?client=gtx&sl=auto&tl=" + targetLang + "&dt=t&q=" + encodeURIComponent(sourceText);
-    
-    $.getJSON(url, function(data) {
-        var translatedContent = '';
-        for (var i=0; i<data[0].length; i++){
-            translatedContent += data[0][i][0];
+    // Skip empty elements
+    if (text.trim() !== '') {
+      translateText(text, function(err, translatedText) {
+        if (!err) {
+          element.textContent = translatedText;
+        } else {
+          console.log('Error translating text:', err);
         }
-        $('.path-frontpage').html(translatedContent);
-    });
-  }
-  
+      });
+    }
+  });
+}
+
+// Run the translation function when the page finishes loading
+window.addEventListener('load', translatePage);
