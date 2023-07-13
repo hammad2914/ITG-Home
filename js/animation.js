5cc5d8e1dd95cfe81f1e2f5d658baed2008bca62
Hammad Ali <ch.hammad2914@gmail.com>
2023-07-13 18:02:22 +0500

    new

--- js/animation.js
+++
@@ -1,8 +0,0 @@
-
-var animation = bodymovin.loadAnimation({
-  container: document.getElementById('lottie-animation'),
-  renderer: 'svg',
-  loop: true,
-  autoplay: true,
-  path: 'js/animation.json'
-});
